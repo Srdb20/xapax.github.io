373ce3f2d1455faabf12a0db6d67952ed7db4918
xapax <philip.linghammar@gmail.com>
2017-02-01 18:04:37 -0300

    added some more challs

--- reveng/script.js
+++ reveng/script.js
@@ -1,33 +1,21 @@
 $( document ).ready(function(){
-
     $('.solution').hide();
     $('.insert_random').hide();
-
     $(".button").click(function() {
         var nameParameter = this.name;
         console.log(nameParameter);
         $('.solution[name= ' + nameParameter + ']').toggle();
-
     });
-
 
     $(".random").click(function() {
         $(".insert_random").empty();
-        // $('.insert_random').show();
         $('.insert_random').fadeOut();
-
         $('.insert_random').fadeIn();
-
-
-
-
 
         var number = $(".exercise_section").length
         var randomNumber = Math.floor(Math.random() * number) + 1;
         randomExercise = ".example" + randomNumber;
         $(randomExercise).clone(true, true).appendTo(".insert_random");
-        // Set border
-        // $(".insert_random").children(".exercise_section").css( "border", "3px double red" );
 
         // Remove header
         $(".insert_random").children(".exercise_section").children("h2").remove();
@@ -38,9 +26,5 @@ $( document ).ready(function(){
         // Change the attributes for the button and solution so that the button works as expected.
         $(".insert_random").children(".exercise_section").children(".solution").attr("name", "randomCode");
         $(".insert_random").children(".exercise_section").children(".button").attr("name", "randomCode");
-
-
     });
-
-
 });
